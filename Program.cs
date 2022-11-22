a0a5c0f38846c35cda10564c5c4534e7dac47657
Marina <marina.gazogareeva@gmail.com>
2022-11-22 17:22:59 +0300

    added task2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.Clear();
+ Console.Write("Введите 1-е число: ");
+  int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
